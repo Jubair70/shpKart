d23fce29d160037881ad3a7878ea04f7bd10fe0c
Jubair Hossain <jubair.hossain@yahoo.com>
2022-10-26 10:13:24 +0600

    Cart Icon & Dropdown

--- client/src/routes/navigation/navigation.component.jsx
+++ client/src/routes/navigation/navigation.component.jsx
@@ -6,6 +6,8 @@ import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import './navigation.styles.scss';
 import { UserContext } from '../../contexts/user.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
 const Navigation = () => {
   const {currentUser} = useContext(UserContext);
@@ -29,8 +31,9 @@ const Navigation = () => {
           </Link>
             )
           }
-          
+          <CartIcon/>
         </div>
+        <CartDropdown/>
       </div>
       <Outlet />
     </Fragment>
